97609155a8949318e517530940a0a7c549d396b0
Radithya Romero Al Giffary <radithya.romero@gmail.com>
2026-08-07 14:28:50 +0700

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+require("dotenv/config");
+
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
@@ -5,7 +7,15 @@ const { notFound, errorHandler } = require("./middlewares/error.middleware");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin(origin, callback) {
+      callback(null, origin === process.env.FRONTEND_URL);
+    },
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 app.use(routes);
